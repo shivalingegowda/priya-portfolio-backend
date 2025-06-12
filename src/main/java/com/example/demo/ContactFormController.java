59b92854398838acb25292ff645cf7133d92edbc
shivalingegowda <shivalinge038@gmail.com>
2025-06-12 22:09:29 +0530

    Enable CORS for contact form

--- src/main/java/com/example/demo/ContactFormController.java
+++ src/main/java/com/example/demo/ContactFormController.java
@@ -3,11 +3,12 @@ package com.example.demo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
+@CrossOrigin(origins = "*")  // Allow all origins (like GitHub Pages)
 @RestController
 @RequestMapping("/api/contact")
 public class ContactFormController {
